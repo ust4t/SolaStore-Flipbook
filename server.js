c20b9a5f1acea3c89a78a73b115584eb86daa3ef
omer-ayhan <om.ayhan247@gmail.com>
2022-04-18 19:06:40 +0300

    feat: :zap: server.js configurations

---
+++ server.js
@@ -0,0 +1,30 @@
+// const { createServer } = require('http');
+// const { parse } = require('url');
+const express = require("express");
+const next = require("next");
+require("dotenv").config();
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = process.env.PORT;
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+// const cors = require('cors');
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.set("trust proxy", 1);
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
+});
